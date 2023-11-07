b01b38478612198563beaf67b143a9524c034f07
sandeshbhusal <mail.sandeshbhusal@gmail.com>
2023-11-07 15:51:42 -0700

    update gitignore, add randomplayer

--- game.java
+++ game.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class game {
     public static void main(String[] args) {
         randomPlayer r1 = new randomPlayer();
@@ -6,8 +11,8 @@ public class game {
         int p1wins = 0;
         int p2wins = 0;
         int draws = 0;
-        for (int i = 0; i < 10000; i++) {
-            switch (tournament(r1, r2)) {
+        for (int i = 0; i < 100; i++) {
+            switch (playAgainst(r1, r2)) {
                 case 0:
                     draws += 1;
                     break;
@@ -26,7 +31,7 @@ public class game {
         System.err.println("Tournament results: " + p1wins + " " + p2wins + " " + draws);
     }
 
-    static public int tournament(player p1, player p2) {
+    static public int playAgainst(player p1, player p2) {
         tic_tac_toe board = new tic_tac_toe();
         p2.board = board;
         p1.board = board;
@@ -39,14 +44,67 @@ public class game {
 
         int winner = -1;
 
+        ArrayList<int[]> localBoards = new ArrayList<>();
+
         while (!board.gameover()) {
-            player p = players[playerindex];
-            playerindex = (playerindex + 1) % 2;
-            board.make_move(playerindex + 1, p.make_move());
+            int[] row = new int[11];
+
+            player p = players[playerindex - 1];
+            playerindex = playerindex % 2 + 1;
+
+            row[0] = playerindex;
+            board.make_move(playerindex, p.make_move());
+
+            for (int t = 0; t < board.board.length; t++) {
+                row[t + 1] = board.board[t];
+            }
+
+            localBoards.add(row);
+
+            // Generate data.
             if (board.gamewin()) {
-                winner = playerindex;
+                if (winner == -1) {
+                    row[10] = 0;
+                    break;
+                }
+
+                if (winner == playerindex) {
+                    row[10] = 1;
+                } else {
+                    row[10] = -1;
+                }
                 break;
             }
+        }
+
+        File datafile = new File("./data.csv");
+
+        try {
+            datafile.createNewFile();
+        } catch (Exception e) {
+        }
+        ;
+
+        try (FileWriter myWriter = new FileWriter("./data.csv", true)) {
+            // Iterate over the entire row, and check if the player won the game or not.
+            for (int[] row : localBoards) {
+                if (winner != -1) {
+                    if (row[0] == winner) {
+                        row[10] = 1;
+                    } else {
+                        row[10] = -1;
+                    }
+                }
+                for (int t : row) {
+                    myWriter.write(t + ", ");
+                }
+
+                myWriter.write('\n');
+            }
+
+            myWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         if (winner >= 0) {
--- randomPlayer.java
+++ randomPlayer.java
@@ -4,9 +4,6 @@ public class randomPlayer extends player{
     @Override
     int make_move() {
         Random rand = new Random();
-        if (this.board.possible_moves().size() == 0) {
-            return -1;
-        }
         int moveindex = rand.nextInt(100) % this.board.possible_moves().size();
         return this.board.possible_moves().get(moveindex);
     } 
